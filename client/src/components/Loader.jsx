78acfbfa1ba2b01cabaf294fdf60804a0db9aaa3
nikolay <n_yozov@hotmail.com>
2022-03-28 14:25:17 -0400

    home page styling

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Loader() {
   return (
-    <div>Loader</div>
+    <div className='flex justify-center items-center py-3'>
+      <div className='animate-spin rounded-full h-32 w-32 border-b-2 border-red-700'/>
+
+
+    </div>
   )
 }
